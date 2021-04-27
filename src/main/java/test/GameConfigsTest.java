66df23eb330d155849749fb6e367bb13c0f95d0e
Bruno Ruiz de Somocurcio <brunords@live.com>
2021-04-27 09:00:28 -0700

    Testing Stubs For GameConfigs, GameProperties, and Difficulty. In Test folder.

---
+++ src/main/java/test/GameConfigsTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameConfigsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getCSS() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getDifficulty() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+}
