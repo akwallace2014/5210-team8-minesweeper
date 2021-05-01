22fd315b87d911bf72fe72e8096d32121ce61dcd
nwu124 <55820966+nwu124@users.noreply.github.com>
2021-04-30 17:52:17 -0700

    Added GameProperties unit tests

--- src/main/java/test/GamePropertiesTest.java
+++ src/main/java/test/GamePropertiesTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import me.dzikimlecz.game.model.GameProperties;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,15 +17,55 @@ class GamePropertiesTest {
     void tearDown() {
     }
 
+    // testing register()
     @Test
-    void register() {
+    void register_key_object_existsInMap() {
+        GameProperties test = new GameProperties();
+        test.register("key", "object");
+        assertEquals("object", test.get("key"));
+    }
+    @Test
+    void register_key_empty_existsInMap() {
+        GameProperties test = new GameProperties();
+        test.register("", "object");
+        assertEquals("object", test.get(""));
+    }
+    @Test
+    void register_key_preexisting_noOverwrite() {
+        GameProperties test = new GameProperties();
+        test.register("key", "object1");
+        test.register("key", "object2");
+        assertEquals("object1", test.get("key"));
     }
 
+    // testing get()
     @Test
-    void get() {
+    void get_key_null_null() {
+        GameProperties test = new GameProperties();
+        assertEquals(null, test.get(null));
+    }
+    @Test
+    void get_key_nonexistent_null() {
+        GameProperties test = new GameProperties();
+        assertEquals(null, test.get("key"));
+    }
+    @Test
+    void get_key_exists_object() {
+        GameProperties test = new GameProperties();
+        test.register("key", "object");
+        assertEquals("object", test.get("key"));
     }
 
+    // testing isEmpty()
     @Test
-    void isEmpty() {
+    void isEmpty_gameProperties_empty_true() {
+        GameProperties test = new GameProperties();
+        assertTrue(test.isEmpty());
+    }
+    @Test
+    void isEmpty_gameProperties_notEmpty_false() {
+        GameProperties test = new GameProperties();
+        test.register("key", "object");
+        assertFalse(test.isEmpty());
     }
 }
